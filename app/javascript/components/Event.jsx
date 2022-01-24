871ae2fae4c6a5921cf878876363c889f1f0b2cc
“bodohbobo” <chloelim22@gmail.com>
2022-01-24 12:23:52 +0800

    fix event page time local end time

--- app/javascript/components/Event.jsx
+++ app/javascript/components/Event.jsx
@@ -230,7 +230,7 @@ function Event() {
                     <p style={{marginBottom: 0}}><strong>Event status: </strong>{calcStatus(event.start_date, event.end_date)}</p>
                     {subject ? subjectTitle() : null}
                     <p style={{marginBottom: 0}}><strong>Event start date: </strong> {new Date(event.start_date.slice(0,23)).toLocaleString()}</p>
-                    <p style={{marginBottom: 0}}><strong>Event end date: </strong>{event.end_date ? new Date(event.end_date.slice(0,23)).toLocaleDateString() : "Not specified"}</p>
+                    <p style={{marginBottom: 0}}><strong>Event end date: </strong>{event.end_date ? new Date(event.end_date.slice(0,23)).toLocaleString() : "Not specified"}</p>
                 </Row>
                 <Accordion>
                     <Accordion.Item eventKey="0">
